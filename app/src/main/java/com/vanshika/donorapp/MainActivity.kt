152de2fff3dead3a70cff2f0ee67f4edd53bf992
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-02-13 22:36:05 +0530

    commit

--- app/src/main/java/com/vanshika/donorapp/MainActivity.kt
+++ app/src/main/java/com/vanshika/donorapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.vanshika.donorapp
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import com.vanshika.donorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding : ActivityMainBinding ?= null
+    var navController : NavController ?= null
+    var appBarConfiguration : AppBarConfiguration ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        navController = findNavController(R.id.host)
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        binding?.bottomNavigation?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navHome -> navController?.navigate(R.id.homeFragment)
+                R.id.navDonate -> navController?.navigate(R.id.donateFragment)
+                R.id.navRequests -> navController?.navigate(R.id.requestsFragment)
+                R.id.navMap -> navController?.navigate(R.id.mapFragment)
+                R.id.navProfile -> navController?.navigate(R.id.profileFragment)
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
